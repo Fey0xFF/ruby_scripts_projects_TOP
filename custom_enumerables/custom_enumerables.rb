a88c8a302da824548c8ad33b7085c4555e836088
Anthony Bendas <anthony.bendas@gmail.com>
2017-11-15 21:56:22 -0700

    added my enum take on .each_with_index and test code

--- custom_enumerables/custom_enumerables.rb
+++ custom_enumerables/custom_enumerables.rb
@@ -4,10 +4,20 @@ module CustomEnum
   #my take on .each()
   def my_each(&block)
     i = 0
-    until i > self.length
+    until i == self.length
       yield(self[i])
       i += 1
     end
+    self
+  end
+
+  def my_each_with_index(&block)
+    i = 0
+    until i == self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self
   end
 
 end
@@ -15,7 +25,20 @@ end
 #include the CustomEnum module
 include CustomEnum
 
-#test code
+#test variables
 array = [5,6,7,8]
-#call my_each on array, it works! :)
+stringarray = ["hello","my","name","is","Feythelus"]
+
+=begin
+#my enum for .each()
 array.my_each {|item| puts item}
+#Ruby's built in enum
+array.each {|item| puts item}
+=end
+
+=begin
+#my enum for .each_with_index()
+stringarray.my_each_with_index {|item, index| puts "#{item}, #{index}"}
+#Ruby's built in enum
+stringarray.each_with_index {|item, index| puts "#{item}, #{index}"}
+=end
