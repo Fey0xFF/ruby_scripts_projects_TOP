fa99202b255ae55245f21789f53b50363e57e90a
Anthony Bendas <anthony.bendas@gmail.com>
2017-11-16 22:08:07 -0700

    completed .my_select

--- custom_enumerables/custom_enumerables.rb
+++ custom_enumerables/custom_enumerables.rb
@@ -19,15 +19,29 @@ module CustomEnum
     self
   end
 
+  #my take on .select() using .my_each() from above
+  def my_select(&block)
+    make = [] if self.class == Array
+    make = {} if self.class == Hash
+    self.my_each do |item|
+      make.push(item) if yield(item) == true
+    end
+    make
+  end
+
+
 end
 
 #include the CustomEnum module
 include CustomEnum
 
-#test variables
+#test variables & code
 array = [5,6,7,8]
 stringarray = ["hello","my","name","is","Feythelus"]
 hashlist = {"name" => "Feythelus", age: 100, 1234 => "one,two,three,four"}
+
+newarray = array.my_select {|item| item > 6}
+print newarray
 
 #array.my_each_with_index {|item, index| print "#{item}, #{index}     "}
 #hashlist.my_each_with_index {|key, val, index| print "#{key}, #{val}, #{index}   "}
